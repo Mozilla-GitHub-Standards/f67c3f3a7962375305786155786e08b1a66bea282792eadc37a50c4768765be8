4a7826df3c64c7ef891a80629107a7aa6ddafc14
Tarek Ziade <tarek@ziade.org>
2014-06-06 14:25:41 +0200

    plugged the root view

--- omxen/__init__.py
+++ omxen/__init__.py
@@ -1,4 +1,11 @@
+from gevent import monkey; monkey.patch_all()
+from bottle import route, run
+
+
+@route('/')
+def index():
+    yield 'OMXEN SMS GATEWAY'
 
 
 def main():
-    pass
+    run(host='0.0.0.0', port=8080, server='gevent')
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 import os
 from setuptools import setup, find_packages
 
-requires = ['bottle']
+requires = ['bottle', 'gevent']
 
 
 setup(name='omxen',
